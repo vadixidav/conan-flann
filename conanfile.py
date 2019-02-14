57705e71dbe356193b9729a12278bf9045d9f0cb
Geordon Worley <vadixidav@gmail.com>
2019-02-14 15:22:34 -0500

    inital commit

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, CMake, tools
+from conans.tools import download, unzip
+import os
+import shutil
+
+
+class FlannConan(ConanFile):
+    name = "flann"
+    version = "1.9.1"
+    license = "BSD"
+    url = "https://github.com/mariusmuja/flann"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    generators = "cmake"
+
+    def source(self):
+        tools.get(
+            "https://github.com/mariusmuja/flann/archive/{}.zip".format(self.version), destination=".")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(
+            source_folder="{}/{}-{}".format(self.source_folder, self.name, self.version))
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
